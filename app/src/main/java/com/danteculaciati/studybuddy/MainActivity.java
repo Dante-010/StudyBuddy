c0b3e4660b8704896f27c9a374b0bc238acb5e1d
Dante Culaciati <programmingdante@gmail.com>
2022-02-16 18:48:43 -0300

    Started working on main activity layout.

--- app/src/main/java/com/danteculaciati/studybuddy/MainActivity.java
+++ app/src/main/java/com/danteculaciati/studybuddy/MainActivity.java
@@ -1,8 +1,11 @@
 package com.danteculaciati.studybuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+
+import com.danteculaciati.studybuddy.Objectives.ObjectiveViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        ObjectiveViewModel model = new ViewModelProvider(this).get(ObjectiveViewModel.class);
+//        // TODO: Add observers to update UI
     }
 }
