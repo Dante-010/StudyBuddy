fc9be7120adb6a5ea86da25c6bbe701764742fb1
Dante Culaciati <programmingdante@gmail.com>
2022-03-04 14:54:54 -0300

    Added settings and objective creation activities.

--- app/src/main/java/com/danteculaciati/studybuddy/MainActivity.java
+++ app/src/main/java/com/danteculaciati/studybuddy/MainActivity.java
@@ -1,5 +1,9 @@
 package com.danteculaciati.studybuddy;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.LiveData;
@@ -8,17 +12,10 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-
 import com.danteculaciati.studybuddy.Objectives.Objective;
-import com.danteculaciati.studybuddy.Objectives.ObjectiveType;
 import com.danteculaciati.studybuddy.Objectives.ObjectiveViewModel;
 import com.danteculaciati.studybuddy.databinding.ActivityMainBinding;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -52,31 +49,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         binding.newObjectiveButton.setOnClickListener(v -> {
-
-
-            Objective objective = new Objective(
-                    "Test",
-                    ObjectiveType.OBJECTIVE_DO,
-                    192,
-                    LocalDate.now().minusDays(1),
-                    LocalDate.now().plusDays(1)
-            );
-
-            model.insertAll(objective);
-
-            Log.d("OBJECTIVE_BUTTON_LISTENER", "New objective called.");
-            if (model.getActive() != null) {
-                model.getActive().observe(this, adapter::setObjectiveList);
-            }
+            Intent intent = new Intent(this, ObjectiveCreationActivity.class);
+            startActivity(intent);
         });
 
-        binding.settingsButton.setOnClickListener(v -> {
-            Log.d("SETTINGS_BUTTON_LISTENER", "Settings button clicked.");
-        });
-/*
-    TODO:
-     - Add settings activity
-     - Add objective creation activity
-*/
+         binding.settingsButton.setOnClickListener(v -> {
+             Intent intent = new Intent(this, SettingsActivity.class);
+             startActivity(intent);
+         });
     }
 }
---
+++ app/src/main/java/com/danteculaciati/studybuddy/ObjectiveCreationActivity.java
@@ -0,0 +1,14 @@
+package com.danteculaciati.studybuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ObjectiveCreationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_objective_creation);
+    }
+}
